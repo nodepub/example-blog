092fffb8ce23195e5099f383b2bcc5a7776a291e
Andrew Gruner <andrew@divineflame.com>
2013-04-23 17:06:54 -0700

    Adding ThemeEngine. Loading its service provider.

--- app/app.php
+++ app/app.php
@@ -24,13 +24,22 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     )
 ));
 
-$app['twig.loader.filesystem']->addPath(__DIR__.'/templates', 'default');
+// $app['twig.loader.filesystem']->addPath(__DIR__.'/templates', 'default');
+
+// TODO: define this dynamically
+$app['twig']->addGlobal('site', array(
+    'name'        => 'NodePub',
+    'tagline'     => 'I Like Turtles',
+    'title'       => 'NodePub Example Blog',
+    'description' => 'This is a dummy site for showing NodePub Blog Engine',
+    'url'         => 'http://nodepub.com'
+));
 
 # ===================================================== #
 #    BLOG CONFIG                                        #
 # ===================================================== #
 
-$app->register(new NodePub\BlogEngine\ServiceProvider(), array(
+$app->register(new NodePub\BlogEngine\Provider\BlogServiceProvider(), array(
     'blog.posts_dir' => __DIR__.'/../posts',
     'blog.template_engine' => $app['twig']
 ));
@@ -49,6 +58,39 @@ $app->register(new NodePub\BlogEngine\ServiceProvider(), array(
 // $app['blog.rss.post_limit']          = 20;
 // $app['blog.rss.template']            = 'rss.twig';
 // $app['blog.recent_posts.post_limit'] = 5;
+
+# ===================================================== #
+#    THEME CONFIG                                       #
+# ===================================================== #
+
+$app->register(new NodePub\ThemeEngine\Provider\ThemeServiceProvider(), array(
+    'np.theme.paths' => realpath(__DIR__.'/../web/themes')
+));
+
+// This is temporary, need a way to abstract customized theme settings
+// so that they could be coming from anywhere - db, static files, etc.
+// We shouldn't care where they are coming from at this point,
+// but we may want to create a default way to add custom settings
+$app['np.theme.settings'] = $app->share(function($app) {
+    return array(
+        'default' => array(
+            'header_bg_color' => '#1D96D7'
+        ),
+
+        // these are future theme ideas:
+        'metropolis' => array(),
+        'gotham' => array(),
+        'carbide' => array(),
+        'leaf' => array(),
+        'grape-sode' => array()
+    );
+});
+
+$app['np.theme.custom_settings'] = $app['np.theme.settings'][$app['np.theme.active']];
+
+# ===================================================== #
+#    ROUTES                                             #
+# ===================================================== #
 
 $app->get('/', function() use ($app) {
     return $app->redirect($app['url_generator']->generate('blog_get_posts'));
