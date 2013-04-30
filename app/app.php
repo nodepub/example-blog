67f343b11d3f9bd94662633b316627052efec2ed
Andrew Gruner <andrew@divineflame.com>
2013-04-30 15:46:00 -0700

    Adding a few more providers, tweaking blog and theme settings.

--- app/app.php
+++ app/app.php
@@ -5,14 +5,27 @@
 
 $loader = require_once __DIR__.'/bootstrap.php';
 
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\EventDispatcher\Event;
+use NodePub\ThemeEngine\ThemeEvents;
 
 $app = new Silex\Application();
 
 $app['debug'] = true;
-
 $app['config_dir'] = __DIR__.'/_config';
+$app['np.admin_mount_point'] = '/np-admin';
+
+// TODO: this is only a stub
+$app['site'] = $app->share(function() {
+    return array(
+        'name'        => 'NodePub Example Blog',
+        'tagline'     => 'A blog engine that WON\'T kidnap and kill you',
+        'title'       => 'NodePub Example Blog',
+        'description' => 'This is a demo site for NodePub Blog Engine',
+        'url'         => 'http://nodepub.com',
+        'ga_code'     => '12345',
+        'theme'       => 'sumatra'
+    );
+});
 
 # ===================================================== #
 #    SERVICE PROVIDERS                                  #
@@ -20,25 +33,24 @@ $app['config_dir'] = __DIR__.'/_config';
 
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
-$app->register(new Silex\Provider\FormServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app->register(new Silex\Provider\FormServiceProvider(), array(
+    'form.secret' => md5('I call the big one Bitey')
+));
+
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'locale_fallback' => 'en',
+));
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.options'    => array(
         'autoescape' => false,
-        // 'cache' => __DIR__.'/_cache'
+        'cache' => false
     )
 ));
 
-// $app['twig.loader.filesystem']->addPath(__DIR__.'/templates', 'default');
-
-// TODO: define this dynamically
-$app['twig']->addGlobal('site', array(
-    'name'        => 'NodePub',
-    'tagline'     => 'I Like Turtles',
-    'title'       => 'NodePub Example Blog',
-    'description' => 'This is a dummy site for showing NodePub Blog Engine',
-    'url'         => 'http://nodepub.com'
-));
+$app['twig']->addGlobal('site', $app['site']);
 
 # ===================================================== #
 #    BLOG CONFIG                                        #
@@ -46,23 +58,31 @@ $app['twig']->addGlobal('site', array(
 
 $app->register(new NodePub\BlogEngine\Provider\BlogServiceProvider(), array(
     'blog.posts_dir' => __DIR__.'/../posts',
-    'blog.template_engine' => $app['twig']
+    'blog.template_engine' => $app['twig'],
+
+    // these are internal templates, move to provider
+    'blog.index.template' => 'post_index.twig',
+    'blog.post.template' => 'post.twig',
 ));
 
-// These are the default configuration values set in the service provider
-// any of them can be overridden here:
+// Listen for theme activation and set the relevant blog templates
+$app['dispatcher']->addListener(ThemeEvents::THEME_ACTIVATE, function(Event $event) use ($app) {
 
-// $app['blog.template.engine']         = $app['twig'];
-// $app['blog.content_filter']          = defaults to markdown;
-// $app['blog.frontpage.post_limit']    = 20;
-// $app['blog.frontpage.template']      = 'blog.twig';
-// $app['blog.permalink.template']      = 'post.twig';
-// $app['blog.default.template']        = 'post.twig';
-// $app['blog.tag_page.template']       = 'blog.twig';
-// $app['blog.category_page.template']  = 'blog.twig';
-// $app['blog.rss.post_limit']          = 20;
-// $app['blog.rss.template']            = 'rss.twig';
-// $app['blog.recent_posts.post_limit'] = 5;
+    // We may want some kind of registry or ThemeTemplateResolver object
+    // that uses theme's configuration to map it's templates to common page types,
+    // otherwise all themes have to use exact names,
+    // and there's no way to share a template for different page types, or fallback on a parent theme
+
+    $name = $event->getTheme()->getNamespace();
+    $app['blog.frontpage.template']      = '@'.$name.'/blog_index.twig';
+    $app['blog.permalink.template']      = '@'.$name.'/blog_post.twig';
+    $app['blog.default.template']        = '@'.$name.'/blog_post.twig';
+    $app['blog.tag_page.template']       = '@'.$name.'/blog_index.twig';
+    $app['blog.category_page.template']  = '@'.$name.'/blog_index.twig';
+    $app['blog.archive.template']        = '@'.$name.'/blog_archive.twig';
+
+    $app['np.theme.templates.custom_css'] = '@'.$name.'/_styles.css.twig';
+});
 
 # ===================================================== #
 #    THEME CONFIG                                       #
@@ -70,8 +90,14 @@ $app->register(new NodePub\BlogEngine\Provider\BlogServiceProvider(), array(
 
 $app->register(new NodePub\ThemeEngine\Provider\ThemeServiceProvider(), array(
     'np.theme.paths' => realpath(__DIR__.'/../web/themes'),
-    'np.theme.custom_settings_file' => $app['config_dir'].'/theme_settings.yml'
+    'np.theme.custom_settings_file' => $app['config_dir'].'/theme_settings.yml', // where settings are saved
+    'np.theme.mount_point' => $app['np.admin_mount_point'].'/themes',
+    'np.theme.active' => $app['site']['theme'],
 ));
+
+// $app['np.theme.fontstack_provider'] = $app->share(function($app) {
+//     $fontStacks = Symfony\Component\Yaml\Yaml::parse($app['config_dir'].'/font_stacks.yml');
+// });
 
 # ===================================================== #
 #    ROUTES                                             #
